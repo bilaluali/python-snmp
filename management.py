6e547a82504287f149a40656a513f27015c84110
bilaluali <bilaluali98@gmail.com>
2020-05-23 12:40:18 +0200

    Created management.py

---
+++ management.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+'''---------------------------------------------------------------
+    * Created on Sat May 23 2020
+    *
+    * GNU License
+    *
+    * Authors:
+    *   - Bilal Uali - https://github.com/bilaluali
+    *   - Aleks Genov Draganova - https://github.com/AleksSG
+    *
+-------------------------------------------------------------------- '''
+
